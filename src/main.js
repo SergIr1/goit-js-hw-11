035e94583d73faf3306434e4d18c26497b7e00ce
Serhii Karskyi <serhiipraktic7@gmail.com>
2025-01-20 15:37:04 +0000

    add librarry

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
+import SimpleLightbox from "simplelightbox";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
+const inputEl = document.querySelector('.form-input');
+
+
+
+// inputEl.addEventListener('focus', () => {
+//     inputEl.setAttribute('placeholder', 'hous|');
+// });
+
+// inputEl.addEventListener('blur', () => {
+//     inputEl.setAttribute('placeholder', 'Search images...');
+// })
