d8b91e9402313ed15214f4ae542cbc7a5676a9d8
Serhii Karskyi <serhiipraktic7@gmail.com>
2025-01-22 14:21:58 +0000

    add

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import SimpleLightbox from "simplelightbox";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import closeModalIcon from "./img/close-modal-btn.svg"
 
 const searchFormEl = document.querySelector('.js-search-form');
 const galleryEl = document.querySelector(".js-gallery");
@@ -9,9 +10,27 @@ const galleryEl = document.querySelector(".js-gallery");
 
 const createGalleryCardTemplate = imgInfo => {
     return `<li class="gallery-card">
-                <img class="gallery-img" src="${imgInfo.webformatURL}" alt="${imgInfo.tags}" width="${imgInfo.webformatWidth=360}" height="${imgInfo.webformatHeight=180}">
-            </li>`
-};
+                <img class="gallery-img" src="${imgInfo.webformatURL}" alt="${imgInfo.tags}" width="${imgInfo.webformatWidth = 360}" height="${imgInfo.webformatHeight = 200}">
+    <ul class="gallery-list">
+      <li class="gallery-item">
+        <h2 class="gallery-title">Likes</h2>
+        <p class="gallery-text">${imgInfo.likes}</p>
+      </li>
+      <li class="gallery-item">
+        <h2 class="gallery-title">Views</h2>
+        <p class="gallery-text">${imgInfo.views}</p>
+      </li>
+      <li class="gallery-item">
+        <h2 class="gallery-title">Comments</h2>
+        <p class="gallery-text">${imgInfo.comments}</p>
+      </li>
+      <li class="gallery-item">
+      <h2 class="gallery-title">Downloads</h2>
+      <p class="gallery-text">${imgInfo.downloads}</p>
+      </li>
+    </ul>         
+                </li>`
+}
 
 const onSearchFormSubmit = event => {
     event.preventDefault();
@@ -24,7 +43,7 @@ const onSearchFormSubmit = event => {
     }
     console.log(searchQuery);
 
-    fetch(`https://pixabay.com/api/?key=48343538-15a5755b500219024f825f792&q=${searchQuery}&image_type=photo`).then(response => {
+    fetch(`https://pixabay.com/api/?key=48343538-15a5755b500219024f825f792&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`).then(response => {
     if (!response.ok) {
         throw new Error(response.status);
     };
@@ -32,8 +51,26 @@ const onSearchFormSubmit = event => {
     return response.json();
     }).then(data => {
         if (data.total === 0) {
-            alert('The image behind this keyword is unknown');
-
+            iziToast.settings({
+    close: true, // Включает крестик
+    closeIconColor: '#FF0000', // Красный цвет крестика по умолчанию
+});
+            iziToast.error({
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                timeout: 3500,
+                position: "topRight",
+                maxWidth: 432,
+                titleColor: "#ffffff",
+                messageColor: "#ffffff",
+                backgroundColor: "#ef4040",
+                close: false,
+                closeIcon: false,
+                closeIconColor: '#ffffff',
+                closeOnEscape: true,
+                closeOnClick: true,
+                icon: 'font-icon',
+                iconUrl: closeModalIcon,
+            });
             galleryEl.innerHTML = "";
 
             searchFormEl.reset();
